cfc375c97cb13188dc7a97bc4b4cdcdcf034ec12
Manuel Abrante <abrantemanuel@gmail.com>
2025-12-29 15:13:57 -0400

    conditionally show elements

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -8,28 +8,43 @@ type ExperienceProps = {
 export const Experience = ({ experience }: ExperienceProps) => {
   return (
     <div className={styles.experienceContainer}>
-      <h2 className={styles.headingRow}>Experience</h2>
-      {experience.map((exp, index) => (
-        <div key={index} className={styles.experienceItem}>
-          <div className={styles.experienceItemHeader}>
-            <div className={styles.experienceItemHeaderCompany}>{exp.company}</div>
-            <div className={styles.experienceItemHeaderPeriod}>
-              {exp.period}
+      {experience?.length > 0 && (
+        <>
+          <h2 className={styles.headingRow}>Experience</h2>
+          {experience.map((exp, index) => (
+            <div key={index} className={styles.experienceItem}>
+              <div className={styles.experienceItemHeader}>
+                <div className={styles.experienceItemHeaderCompany}>
+                  {exp.company}
+                </div>
+                <div className={styles.experienceItemHeaderPeriod}>
+                  {exp.period}
+                </div>
+              </div>
+              <div className={styles.experienceItemContent}>
+                <div className={styles.experienceItemPosition}>
+                  {exp.position}
+                </div>
+                <ul className={styles.experienceItemResponsibilities}>
+                  {exp.responsibilities.map((resp, idx) => (
+                    <li
+                      key={idx}
+                      className={styles.experienceItemResponsibility}
+                    >
+                      <span
+                        className={styles.experienceItemResponsibilityBullet}
+                      >
+                        -
+                      </span>
+                      <span>{resp}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-          <div className={styles.experienceItemContent}>
-            <div className={styles.experienceItemPosition}>{exp.position}</div>
-            <ul className={styles.experienceItemResponsibilities}>
-              {exp.responsibilities.map((resp, idx) => (
-                <li key={idx} className={styles.experienceItemResponsibility}>
-                  <span className={styles.experienceItemResponsibilityBullet}>-</span>
-                  <span>{resp}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ))}
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/knowledgeAndTools/KnowledgeAndTools.tsx
+++ src/components/knowledgeAndTools/KnowledgeAndTools.tsx
@@ -25,53 +25,76 @@ export const KnowledgeAndTools = ({
 }: KnowledgeAndToolsProps) => {
   return (
     <div className={styles.knowledgeAndToolsContainer}>
-      <h2 className={styles.headingRow}>Knowledge and Tools</h2>
-      <div className={styles.skillsContainer}>
-        <Skill skills={knowledgeAndTools?.primary || []} />
-        <Skill skills={knowledgeAndTools?.secondary || []} />
-      </div>
-
-      <h2 className={styles.headingRow}>Languages</h2>
-      <div className={styles.skillsContainer}>
-        <Skill
-          skills={languages.map((language) => ({
-            name: language.language,
-            level: Number(language.level),
-          }))}
-        />
-      </div>
-
-      <h2 className={styles.headingRow}>Courses</h2>
-      <div className={styles.coursesContainer}>
-        {courses.map((course, index) => (
-          <div className={styles.courseContainer} key={`${index}-${course.institution}`}>
-            <div className={styles.bulletItem}></div>
-            <div className={styles.courseContent}>
-              <div className={styles.courseItem}>
-                <h3>{course.institution}</h3>
-                <p className={styles.courseYear}>{course.year}</p>
-              </div>
-              <p className={styles.courseCourse}>{course.course}</p>
+      {
+        (knowledgeAndTools?.primary?.length > 0 || knowledgeAndTools?.secondary?.length > 0) && (
+          <>
+            <h2 className={styles.headingRow}>Knowledge and Tools</h2>
+            <div className={styles.skillsContainer}>
+              <Skill skills={knowledgeAndTools?.primary || []} />
+              <Skill skills={knowledgeAndTools?.secondary || []} />
             </div>
-          </div>
-        ))}
-      </div>
+          </>
+        )}
 
-      <h2 className={styles.headingRow}>Education</h2>
-      <div className={styles.coursesContainer}>
-        {education.map((education, index) => (
-          <div className={styles.courseContainer} key={`${index}-${education.institution}`}>
-            <div className={styles.bulletItem}></div>
-            <div className={styles.courseContent}>
-              <div className={styles.courseItem}>
-                <h3>{education.institution}</h3>
-                <p className={styles.courseYear}>{education.period}</p>
-              </div>
-              <p className={styles.courseCourse}>{education.degree}</p>
-            </div>
+      {languages && languages.length > 0 && (
+        <>
+          <h2 className={styles.headingRow}>Languages</h2>
+          <div className={styles.skillsContainer}>
+            <Skill
+              skills={languages.map((language) => ({
+                name: language.language,
+                level: Number(language.level),
+              }))}
+            />
           </div>
-        ))}
-      </div>
-    </div >
+        </>
+      )}
+
+      {courses && courses?.length > 0 && (
+        <>
+          <h2 className={styles.headingRow}>Courses</h2>
+          <div className={styles.coursesContainer}>
+            {courses.map((course, index) => (
+              <div
+                className={styles.courseContainer}
+                key={`${index}-${course.institution}`}
+              >
+                <div className={styles.bulletItem}></div>
+                <div className={styles.courseContent}>
+                  <div className={styles.courseItem}>
+                    <h3>{course.institution}</h3>
+                    <p className={styles.courseYear}>{course.year}</p>
+                  </div>
+                  <p className={styles.courseCourse}>{course.course}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+
+      {education && education?.length > 0 && (
+        <>
+          <h2 className={styles.headingRow}>Education</h2>
+          <div className={styles.coursesContainer}>
+            {education.map((education, index) => (
+              <div
+                className={styles.courseContainer}
+                key={`${index}-${education.institution}`}
+              >
+                <div className={styles.bulletItem}></div>
+                <div className={styles.courseContent}>
+                  <div className={styles.courseItem}>
+                    <h3>{education.institution}</h3>
+                    <p className={styles.courseYear}>{education.period}</p>
+                  </div>
+                  <p className={styles.courseCourse}>{education.degree}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
